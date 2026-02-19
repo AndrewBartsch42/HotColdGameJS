441dca4afbe33c4416fe17762c3dee1ca8c93d08
andrewbartsch42 <andrewbartsch01@gmail.com>
2026-02-19 15:12:14 -0600

    fixed bug where switch logic would only use default

--- guess.js
+++ guess.js
@@ -4,6 +4,10 @@
 let randomNum = 0;
 let tries = 0;
 
+
+//dom references
+const history = document.getElementById("history");
+const messageREF = document.getElementById("message");
 // helper function
 const getRandomInt = (max = 100) => {
     let num = Math.random() * max;  // get a random number between 0 and max
@@ -14,7 +18,7 @@ const getRandomInt = (max = 100) => {
 // event handler functions
 const guessClick = () => {
     const guess = parseInt(document.querySelector("#number").value);
-
+    let color = "";
     let message = "";
     if (isNaN(guess)) {
         message = "Not a valid number. Please enter a valid number."
@@ -24,42 +28,49 @@ const guessClick = () => {
         return;
     }
     let distance = Math.abs(guess - randomNum);
-    switch(distance){
-        case (Math.abs(distance) === 0):
+    switch(true){
+        case (distance === 0):
             const lastWord = (tries === 1) ? "try" : "tries";
             message = `Fire! You guessed it in ${tries} ${lastWord}!`;
             color = 'green';
-            updatedBestScore();
+            updatedBestScore(tries);
             break;
         case (distance <= 5):
+            console.log("hot");
             message = "Hot! (Within 5)";
             color = "red";
             break;
         case (distance <= 10):
+            console.log("warmer");
             message = "Warmer (Within 10)";
             color = "orangered";
             break;
         case (distance <= 20):
+            console.log("warm");
             message = "Warm (Within 20)";
             color = "orange";
             break;
         case (distance <= 30):
+            console.log("cold");
             message = "Cold (Within 30)";
             color = "lightblue";
             break;
         case (distance <= 40):
-            message = "Colder (Within 5)";
+            console.log("colder");
+            message = "Colder (Within 40)";
             color = "blue";
             break;
-        default:
-            message = "Freezing (Within 5)";
+        case (distance <= 50):
+            console.log("freezing");
+            message = "Freezing (Way off)";
             color = "darkblue";
             break;
 
     }
+    tries += 1;
     history.innerHTML += `Guess ${tries}; ${guess} - ${message}<br>`;
-    
-    document.querySelector("#message").textContent = message;
+    messageREF.textContent = message;
+    messageREF.style.color = color;
 };
 
 const playAgainClick = () => {
